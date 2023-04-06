41d060eee148d8271d2178787c5462465219de9f
Tim Zimmermann <tim@linux4.de>
2023-04-06 20:14:03 +0200

    x1s: Replace libutils-v32 with a shim
    Change-Id: Ia4d8aca9de61cf17f9c34cf001f6eef723025e75

--- extract-files.sh
+++ extract-files.sh
@@ -19,6 +19,10 @@ function blob_fixup() {
             xxd -r -p "${2}".hex > "${2}"
             rm "${2}".hex
             ;;
+        vendor/lib*/sensors.*.so)
+            "${PATCHELF}" --replace-needed libutils.so libutils-v32.so "${2}"
+            sed -i 's/_ZN7android6Thread3runEPKcim/_ZN7utils326Thread3runEPKcim/g' "${2}"
+            ;;
     esac
 }
 
